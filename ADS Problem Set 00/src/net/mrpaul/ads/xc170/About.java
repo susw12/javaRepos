64544e4cf4fbd67d7f9e96c52fbb86a76d5029cf
susw12 <sujaymswain@gmail.com>
2018-02-07 17:21:39 -0500

    All This stuff

--- ADS Problem Set 00/src/net/mrpaul/ads/xc170/About.java
+++ ADS Problem Set 00/src/net/mrpaul/ads/xc170/About.java
@@ -1,7 +1,11 @@
 package net.mrpaul.ads.xc170;
 
+import java.math.*;
+
 public class About {
     public static void main(String[] args){
-        System.out.println("Hello");    
+        System.out.println("Hello Mr. Gonzalez and Mr. Paul");
+        System.out.print("My name is Sujay Swain.\nI am 14 years old.");
+        System.out.println("Outside of school I go to hackathons. I ");
     }
 }
