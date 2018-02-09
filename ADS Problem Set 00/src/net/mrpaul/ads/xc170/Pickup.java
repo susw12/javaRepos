646c3789002dfc142cb512b0ef4d221b3976fe5f
susw12 <sujaymswain@gmail.com>
2018-02-08 23:24:10 -0500

    Hello

---
+++ ADS Problem Set 00/src/net/mrpaul/ads/xc170/Pickup.java
@@ -0,0 +1,15 @@
+package net.mrpaul.ads.xc170;
+
+import java.util.Scanner;
+
+
+public class Pickup {
+    public static void main(String[] args) {
+        Scanner getName = new Scanner(System.in);
+        System.out.print("What is this person's name? ");
+        String name = getName.next();
+        System.out.println(name);
+
+
+    }
+}
