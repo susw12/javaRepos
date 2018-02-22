af76fcd36a7ce89e02052ab00b701d67c1fcaccf
susw12 <sujaymswain@gmail.com>
2018-02-22 09:11:10 -0500

    Herro

---
+++ Problem Set 02/ps02_Distribution Files_0/ps02_Distribution Files/Exercises.java
@@ -0,0 +1,74 @@
+/*
+ * 
+ * 
+ * 
+ */
+package net.mrpaul.ads.csid.ps02;
+
+import java.util.Scanner;
+import java.util.*;
+import java.lang.Math;
+
+public class Exercises {
+
+	public static void main(String[] args){
+		
+		/* Method calls.  Un-comment any line to make that method run.  
+		 * Note: When you click run, the main method is the one that runs.
+		 * That means that each method that you uncomment will run sequentially.
+		 * If you were to include repeat calls, the method would run multiple times.
+		 */
+		
+		//tempChecker();
+		//guardDog();
+		//coinFlip();
+		//rollDice();
+		//quadraticFormula(1.0, 1.0, -4.0);  //see this?  These are arguments, not Scanner input
+		//guessMyNumberLimited();
+		//guessMyNumberUnlimited();
+	}//end of Exercises main method
+	
+	
+	//tempChecker()
+	public static void tempChecker(){
+		
+		
+	}
+	
+	//guardDog()
+	public static void guardDog(){
+		
+	
+	}
+	
+	
+	//coinFlip()
+	public static void coinFlip(){
+	
+	}
+	
+	//rollDice()
+	public static void rollDice(){
+		
+	}
+	
+	
+	//quadraticFormula()
+	public static void quadraticFormula(double a, double b, double c){
+		
+	}
+	
+	//guessMyNumberLimited()
+	public static void guessMyNumberLimited(){
+		
+		
+	}
+	
+	//guessMyNumberUnlimited()
+	public static void guessMyNumberUnlimited(){
+		
+		
+	}
+	
+	
+}
---
+++ Problem Set 02/ps02_Distribution Files_0/ps02_Distribution Files/Greedy.java
@@ -0,0 +1,49 @@
+/*
+ * 
+ * 
+ * 
+ */
+package net.mrpaul.ads.csid.ps02;
+
+import java.util.Scanner;
+
+public class Greedy {
+
+	public static void main(String[] args) {
+
+	    //NO NEED TO MODIFY ANYTHING UNTIL THE COMMENT INDICATING THE START OF YOUR CODE
+	    //declare the variables we'll need
+	    Scanner sc = new Scanner(System.in);
+	    double changeToGive = 0;
+	    
+	    //This is the start of the loop to prompt for the dollar amount
+	    do {
+	    	//if the user gave us a negative dollar amount
+	        if(changeToGive < 0)
+	            System.out.println("Dollar amounts must be positive.");
+	        
+	        //if the current amount is zero dollars
+	        else if(changeToGive == 0)
+	            System.out.println("How much change do you need to pay out?");
+	        
+	        // while the user's input is not a valid double, prompt again
+	        while (!sc.hasNextDouble()) {
+	            System.out.println("Invalid input.  How much change do you need to pay out?");
+	            sc.next(); // this is important!
+	        }
+	        
+	        //if the user entered a valid double, assign it to changeToGive
+	        changeToGive = sc.nextDouble();
+	    } while (changeToGive <= 0); //if the user entered a valid double but it was negative, do it all again
+		
+	    
+	    //HERE IS WHERE YOUR CODE SHOULD GO.
+	    //Start handling changeToGive here.
+		
+	}
+	
+	
+	
+	
+
+}
