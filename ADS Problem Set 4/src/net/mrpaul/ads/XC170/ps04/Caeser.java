79a5edb6baf419f3d91595cf8750847f3bf46dca
Sujay Swain <sujaymswain@gmail.com>
2018-03-10 00:15:17 -0500

    More

--- ADS Problem Set 4/src/net/mrpaul/ads/XC170/ps04/Caeser.java
+++ ADS Problem Set 4/src/net/mrpaul/ads/XC170/ps04/Caeser.java
@@ -2,6 +2,27 @@ package net.mrpaul.ads.XC170.ps04;
 import java.util.Scanner;
 
 public class Caeser {
+    static Scanner getText = new Scanner(System.in);
     public static void main(String[] args) {
+        int keyShift = Integer.parseInt(args[0]);
+        int keyShiftFinal = keyShift % 26;
+        String preConvText = getText.nextLine();
+        int convertToASCIIValue;
+        char convertToString;
+
+        for (int x = 0; x < preConvText.length(); x++){ 
+            if (Character.isUpperCase(preConvText.charAt(x))) {
+                convertToASCIIValue = (int) preConvText.charAt(x);
+                convertToASCIIValue -= 65;
+                convertToASCIIValue = (convertToASCIIValue + keyShift) % 26;
+                convertToASCIIValue += 65;
+                convertToString = (char) convertToASCIIValue;
+                char[] preConvTextArray = preConvText.toCharArray();
+                preConvTextArray[x] = convertToString;
+                preConvText = String.valueOf(preConvTextArray);
+            }
+        }
+        System.out.println(preConvText);
+
     }
 }
