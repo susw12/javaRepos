3b96a0080c7853712b33da1aafcd9e88072f9dfa
Sujay Swain <sujaymswain@gmail.com>
2018-03-12 08:28:45 -0400

    Vigenere

--- ADS Problem Set 4/src/net/mrpaul/ads/XC240/ps04/Vigenere.java
+++ ADS Problem Set 4/src/net/mrpaul/ads/XC240/ps04/Vigenere.java
@@ -1,11 +1,17 @@
 package net.mrpaul.ads.XC240.ps04;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Arrays;
+
 
 
 public class Vigenere {
     static Scanner getText = new Scanner(System.in);
     public static void main(String[] args) {
         String argsVar = args[0];
-        System.out.println(argsVar);
+        argsVar.trim();
+        List<String> argsList = Arrays.asList(argsVar.split(""));
+        System.out.println(argsList);
+        String decodedText = getText()
     }
 }
