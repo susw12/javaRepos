7b9bb9cc35a46b45e268113de0f14115a2347370
Sujay Swain <sujaymswain@gmail.com>
2018-03-15 00:09:28 -0400

    More

--- ADS Problem Set 4/src/net/mrpaul/ads/XC240/ps04/Caeser.java
+++ ADS Problem Set 4/src/net/mrpaul/ads/XC240/ps04/Caeser.java
@@ -1,14 +1,35 @@
 package net.mrpaul.ads.XC240.ps04;
 import java.util.Scanner;
 
+/**
+*A simple Caeser encoder
+*
+*A simple Caeser encoder using CLI inputs
+*<p>
+*ADS PS04: Crypto
+*3/14/18
+* 
+*
+*@author Sujay Swain
+*/
+
 public class Caeser {
-    static Scanner getText = new Scanner(System.in);
+    static Scanner getText = new Scanner(System.in); //defining scanner as a global
+    /**
+    * Summary fragment: Simple Caeser encoder
+    * A very quick Caeser encoder in Java
+    * @author Sujay Swain
+    * @param args user enter adn a single integer CLI input
+    * @return final values for encode entry
+    */
     public static void main(String[] args) {
+        //getting cli inputs and intitating vars
         int keyShift = Integer.parseInt(args[0]);
         int keyShiftFinal = keyShift % 26;
         String preConvText = getText.nextLine();
         int convertToASCIIValue;
         char convertToString;
+        //checks for uppcase
         for (int x = 0; x < preConvText.length(); x++){ 
             if (Character.isUpperCase(preConvText.charAt(x))) {
                 convertToASCIIValue = (int) preConvText.charAt(x);
@@ -19,7 +40,8 @@ public class Caeser {
                 char[] preConvTextArray = preConvText.toCharArray();
                 preConvTextArray[x] = convertToString;
                 preConvText = String.valueOf(preConvTextArray);
-            }
+            }//end of if
+            //checks for lowercase
             else if (Character.isLowerCase(preConvText.charAt(x))) {
                 convertToASCIIValue = (int) preConvText.charAt(x);
                 convertToASCIIValue -= 97;
@@ -29,12 +51,13 @@ public class Caeser {
                 char[] preConvTextArray = preConvText.toCharArray();
                 preConvTextArray[x] = convertToString;
                 preConvText = String.valueOf(preConvTextArray);
-            }
+            }//end of else if
+            //checks for everything else
             else {
             //Do nothing
-            }
+            }//end of else
         }
         System.out.println(preConvText);
 
-    }
-}
+    }//end of main
+}//end of Caeser
--- ADS Problem Set 4/src/net/mrpaul/ads/XC240/ps04/Vigenere.java
+++ ADS Problem Set 4/src/net/mrpaul/ads/XC240/ps04/Vigenere.java
@@ -2,43 +2,63 @@ package net.mrpaul.ads.XC240.ps04;
 import java.util.List;
 import java.util.Scanner;
 
-import java.util.Arrays;
+/**
+*A simple Vigenere encoder
+*
+*A simple Vigenere encoder using CLI inputs
+*<p>
+*ADS PS04: Crypto
+*3/14/18
+* 
+*
+*@author Sujay Swain
+*/
 
-
-
-public class Vigenere {
-    static Scanner getText = new Scanner(System.in);
+public class Vigenere{
+    static Scanner getText = new Scanner(System.in); //defining scanner as a global
+    /**
+    * Summary fragment: Simple Vigenere encoder
+    * A very quick Vigenere encoder in Java
+    * @author Sujay Swain
+    * @param args user enter adn a single string CLI input
+    * @return final values for encode entry
+    */
     public static void main(String[] args) {
-
-        String argsVar = args[0];
-        argsVar.trim();
-        List<String> argsList = Arrays.asList(argsVar.split(""));
-        System.out.println(argsList);
-        String decodedText = getText.nextLine();
-        List<String> decodedTexList = Arrays.asList(decodedText.split(""));
-        int x;
-        String finalEncode;
-        int extraSpaces = 0;
-        for (x = 0; x < decodedText.length(); x++) {
-            if ((argsVar.charAt(x)) == ' '){
-                extraSpaces++;
-            }
-            else { 
-                int gettingInt = (x-extraSpaces) % argsVar.length();
-                int charInt = Integer.parseInt(decodedTexList.get(x));
-                if (charInt > 95) {
-                    charInt -= 96;
-                    gettingInt -= 96;
-                    charInt = (charInt + gettingInt) % 26;
-                    charInt += 96;
-                    
-                }
-
-                
-
-
-                
-            }
-        }
-    }
-}
+        if(args.length > 1 || args.length < 1) {
+            System.out.println("Only one argument!");
+        }//end if
+        else {
+            //declares variables
+            String decodeKey = args[0];
+            decodeKey = decodeKey.toLowerCase();
+            int placeOnKey = 0, key, keyLength = decodeKey.length();
+            System.out.println("What do you want to encrypt?");
+            String decodedText = getText.nextLine();
+            int positionPlace = decodedText.length();
+            String encodedText = "";
+            
+            //converting char by char with Caeser Cipher
+            for (int i=0; i<positionPlace; i++) {
+                char changeChar = decodedText.charAt(i);
+                if (Character.isAlphabetic(changeChar)) {
+                    int ascii = (int) changeChar;
+                    key = (int) decodeKey.charAt(placeOnKey);
+                    key -= 97;
+                    //when lower case
+                    if (Character.isLowerCase(changeChar)) {
+                        ascii = (key + ascii - 97) % 26 + 97;
+                    }//end if
+                    //whn upper case
+                    else {
+                        ascii = (key + ascii - 65) % 26 + 65;
+                    }//end else
+                    changeChar = (char) ascii;
+                    placeOnKey = (placeOnKey + 1) % keyLength;
+                }//end if
+                encodedText += changeChar;
+            }//end for loop
+        //Print the text
+        System.out.println(encodedText);
+        }//end main else statement
+    }//end main
+}//end Vigenere
