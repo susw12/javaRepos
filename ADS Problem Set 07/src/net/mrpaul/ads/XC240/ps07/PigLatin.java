a9f2cb9fcba9bb112627fbe43674086b777ced6f
Sujay Swain <sujaymswain@gmail.com>
2018-05-01 23:11:46 -0400

    Pset 7

--- ADS Problem Set 07/src/net/mrpaul/ads/XC240/ps07/PigLatin.java
+++ ADS Problem Set 07/src/net/mrpaul/ads/XC240/ps07/PigLatin.java
@@ -7,10 +7,9 @@ public class PigLatin {
     static char[] consonants = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'};
 
     public static void main(String[] args) {
-        startOfFirstConsonantCluster("ADSA");
-        System.out.println("SECOND");
-        startOfFirstConsonantCluster("lajskfd");
+        startOfFirstConsonantClusterMethod("ADSA");
 
+        startOfFirstConsonantClusterMethod("lajskfd");
     }
 
     public static boolean yConso(String string1) {
@@ -38,56 +37,76 @@ public class PigLatin {
     }
 
     public static int startOfFirstConsonantClusterMethod(String string1) {
-
         boolean yConso = yConso(string1);
-        string1.toLowerCase();
         boolean returned = false;
-        //Checks for vowels
-        for (int x = 0; x < string1.length(); x++) {
+        int finalValue = 0;
+        string1.toLowerCase();
+        for (int x = string1.length(); x >= 0; x++) {
             if (yConso) {
                 if (string1.charAt(x) == 'y') {
-                    return (x);
+                    finalValue = x;
                     returned = true;
                     break;
                 }
             } else {
                 for (int n = 0; true; n++) {
                     if ((consonants[x]) == (string1.charAt(x))) {
-                        return (x);
+                        finalValue = x;
                         returned = true;
                         break;
                     }
                 }
             }
             if (!returned) {
-                System.out.println(-1);
+                finalValue = -1;
             }
         }
+        return finalValue;
     }
 
     public static int endOfFirstConsonantClusterMethod(String string1) {
         boolean yConso = yConso(string1);
         boolean returned = false;
+        int finalValue = 0;
         string1.toLowerCase();
         for (int x = string1.length(); x >= 0; x--) {
             if (yConso) {
                 if (string1.charAt(x) == 'y') {
-                    System.out.println(x);
+                    finalValue = x;
                     returned = true;
                     break;
                 }
             } else {
                 for (int n = 0; true; n++) {
                     if ((consonants[x]) == (string1.charAt(x))) {
-                        System.out.println(x);
+                        finalValue = x;
                         returned = true;
                         break;
                     }
                 }
             }
             if (!returned) {
-                System.out.println(-1);
+                finalValue = -1;
             }
         }
+        return finalValue;
+    }
+
+    public String stringToPiglatin(String word) {
+        String english = word;
+        String pigLatin = "";
+        if (startOfFirstConsonantClusterMethod(english) != 0) {
+            pigLatin = english + "way";
+        }
+
+        else {
+            int removal = startOfFirstConsonantClusterMethod(english);
+            //pigLatin =
+        }
+
+
+
+
+        return pigLatin;
     }
 }
