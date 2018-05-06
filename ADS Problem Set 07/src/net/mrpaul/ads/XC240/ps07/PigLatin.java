560f3e117562fcdcb58fb8ee48e0104561f799ee
Sujay Swain <sujaymswain@gmail.com>
2018-05-06 14:08:09 -0400

    Almost all of it

--- ADS Problem Set 07/src/net/mrpaul/ads/XC240/ps07/PigLatin.java
+++ ADS Problem Set 07/src/net/mrpaul/ads/XC240/ps07/PigLatin.java
@@ -1,126 +1,219 @@
 package net.mrpaul.ads.XC240.ps07;
 
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
+/**
+ *
+ */
 public class PigLatin {
-    static char[] consonants = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'};
-    static char[] vowels = {'a', 'e', 'i', 'o', 'u'};
 
-    public static void main(String[] args) {
-        System.out.println(startOfFirstConsonantClusterMethod("ADSA"));
-
-        System.out.println(endOfFirstConsonantClusterMethod("pry"));
+    public static void main(String[] args) throws FileNotFoundException {
+        // TODO Auto-generated method stub
+        //fileToPigLatin("text_to_latin.txt");
     }
+    public static int startOfFirstConsonantCluster(String s) {
+        int length = s.length();
+        int index = length+2;
+        s = s.toLowerCase();
 
-    public static boolean yConso(String string1) {
-        string1 = string1.toLowerCase();
-        //Checks to see if y is a vowel
-        boolean yConso = false;
-        for (int x = 0; x < string1.length(); x++) {
-            if (string1.charAt(x) == ('a')) {
-                yConso = true;
+        for(int i = 0; i < length; i++) {
+            try
+            {
+                // checking valid integer using parseInt() method
+                Integer.parseInt(s);
                 break;
-            } else if (string1.charAt(x) == 'e') {
-                yConso = true;
-                break;
-            } else if (string1.charAt(x) == 'i') {
-                yConso = true;
-                break;
-            } else if (string1.charAt(x) == 'o') {
-                yConso = true;
-                break;
-            } else if (string1.charAt(x) == 'u') {
-                yConso = true;
-                break;
-            } else {
-                yConso = false;
             }
-        }
-
-        return yConso;
-
-    }
-
-    public static int startOfFirstConsonantClusterMethod(String string1) {
-        boolean yConso = yConso(string1);
-        boolean returned = false;
-        int finalValue = 0;
-        string1 = string1.toLowerCase();
-        for (int x = 0; x < string1.length(); x++) {
-            if (!yConso) {
-                if (string1.charAt(x) == 'y') {
-                    finalValue = x-1;
-                    returned = true;
+            catch (NumberFormatException e)
+            {
+                if(s.charAt(i) != 'a'&&s.charAt(i) != 'e'&&s.charAt(i)!='i'&&s.charAt(i)!='o'&&s.charAt(i)!='u') {
+                    index = i;
+                }
+                //if y
+                if(s.charAt(i) == 'y') {
+                    //if y is first letter
+                    if(i == 0 && s.charAt(i) == 'y') {
+                        //if 2nd letter is vowel, y is consonant
+                        if(s.charAt(i + 1)=='a'||s.charAt(i + 1)=='e'||s.charAt(i + 1)=='i'||s.charAt(i + 1)=='o'||s.charAt(i + 1)=='u') {
+                            index = 0;
+                        }
+                        else if(i > 0 && i < length-1) {
+                            //if letter before and after y are NOT consonants, y is consontant
+                            if(!(s.charAt(i - 1)!='a'&&s.charAt(i - 1)!='e'&&s.charAt(i - 1)!='i'&&s.charAt(i - 1)!='o'&&s.charAt(i - 1)!='u' &&
+                                    s.charAt(i + 1)!='a'&&s.charAt(i + 1)!='e'&&s.charAt(i + 1)!='i'&&s.charAt(i + 1)!='o'&&s.charAt(i + 1)!='u')) {
+                                index = i;
+                            }
+                        }
+                    }//end if
+                }//end if
+                //if index got changed
+                if(index != length+2) {
                     break;
                 }
+            }//end catch
+
+        }//end for
+        if(s.equals("") || index == length+2) {
+            index = -1;
+        }
+        //vowel when surrounded by consonants or at end of word
+        return index;
+    }
+
+    public static int endOfFirstConsonantCluster(String s) {
+        int length = s.length();
+        int index = length+2;
+        boolean counter = false;
+        int c = 0;
+        s = s.toLowerCase();
+
+        for(int i = 0; i < length; i++) {
+            try
+            {
+                // checking valid integer using parseInt() method
+                Integer.parseInt(s);
+                break;
             }
-            else {
-                for (int n = 0; n < consonants.length; n++) {
-                    if ((consonants[n]) == (string1.charAt(x))) {
-                        finalValue = x-1;
-                        returned = true;
+            catch (NumberFormatException e)
+            {
+                //if not vowel
+                if(s.charAt(i) != 'a'&&s.charAt(i) != 'e'&&s.charAt(i)!='i'&&s.charAt(i)!='o'&&s.charAt(i)!='u') {
+                    //if y
+                    if(s.charAt(i) == 'y') {
+                        //if y is first letter
+                        if(i == 0 && s.charAt(i) == 'y') {
+                            //if 2nd letter is vowel, y is consonant
+                            if(s.charAt(i + 1)=='a'||s.charAt(i + 1)=='e'||s.charAt(i + 1)=='i'||s.charAt(i + 1)=='o'||s.charAt(i + 1)=='u') {
+                                counter = true;
+                                index = i;
+                                c += 1;
+                            }
+                        }//end if
+                        else if(i > 0 && i < length-1) {
+                            //if letter before and after y are NOT consonants, y is consontant
+                            if((s.charAt(i - 1)=='a'||s.charAt(i - 1)=='e'||s.charAt(i - 1)=='i'||s.charAt(i - 1)=='o'||s.charAt(i - 1)=='u') ||(
+                                    s.charAt(i + 1)=='a'||s.charAt(i + 1)=='e'||s.charAt(i + 1)=='i'||s.charAt(i + 1)=='o'||s.charAt(i + 1)=='u')) {
+                                counter = true;
+                                index = i;
+                                c += 1;
+                            }
+                            else {
+                                counter = false;
+                                break;
+                            }
+                        }//end else if
+                    }//end main y if
+                    else if(s.charAt(i)!='y') {
+                        counter = true;
+                        index = i;
+                        c += 1;
+                    }
+                }//end main if
+                //if is a vowel
+                else {
+                    if(c == 0) {
+                        counter = true;
+                    }
+                    else {
+                        counter = false;
                         break;
                     }
                 }
-            }
-            if (returned) {
-                break;
-            }
+            }//end catch
+        }//end for
+        if(s.equals("") || index == length+2) {
+            index = -1;
         }
-        if (!returned) {
-            finalValue = -1;
-        }
-        return finalValue;
-    }
+        //vowel when surrounded by consonants or at end of word
+        return index;
+    }//end endOfFirstConsonantCluster
 
-    public static int endOfFirstConsonantClusterMethod(String string1) {
-        boolean yConso = yConso(string1);
-        boolean returned = false;
-        int finalValue = 0;
-        string1 = string1.toLowerCase();
+    public static String wordToPigLatin(String s) {
+        String newS, subString;
+        int cluster;
+        s = s.trim();
+        s = s.toLowerCase();
+        newS = s;
+        int length = s.length();
+        char[] vowel = {'a', 'e', 'i', 'o', 'u'};
+        char letter = s.charAt(0);
 
-        for (int x = 0; x < string1.length(); x++) {
-            if (!yConso) {
-                if (string1.charAt(x) == 'y') {
-                    finalValue = x;
-                    returned = true;
-                    break;
-                }
-            }
-            else {
-                for (int n = 0; n < vowels.length; n++) {
-                    if ((vowels[n]) == (string1.charAt(x))) {
-                        finalValue = x;
-                        returned = true;
-                        break;
-                    }
-                }
-            }
-            if (returned) {
-                break;
-            }
+        //if begin with vowel
+        if(letter == 'a'||letter=='e'||letter=='i'||letter=='o'||letter=='u'||letter=='A'||
+                letter=='E'||letter=='I'||letter=='O'||letter=='U') {
+            newS += "way";
         }
-        if (!returned) {
-            finalValue = -1;
-        }
-        return finalValue;
-    }
-
-    public String stringToPiglatin(String word) {
-        String english = word;
-        String pigLatin = "";
-        if (startOfFirstConsonantClusterMethod(english) != 0) {
-            pigLatin = english + "way";
+        else if((letter == 'Y' || letter=='y') && (s.charAt(1)!='a'&&s.charAt(1)!='e'&&s.charAt(1)!='i'&&s.charAt(1)!='o'&&s.charAt(1)!='u'&&s.charAt(1)!='A'
+                &&s.charAt(1)!='E'&&s.charAt(1)!='I'&&s.charAt(1)!='O'&&s.charAt(1)!='U')) {
+            newS += "way";
         }
 
+        //if begin with consonant
         else {
-            int removal = startOfFirstConsonantClusterMethod(english);
-            pigLatin =
+            cluster = endOfFirstConsonantCluster(s);
+            subString = s.substring(0, cluster+1);
+            //remove beginning consonant cluster
+            newS = newS.replace(subString, " ");
+            newS = newS.trim();
+            newS = newS + subString + "ay";
+        }
+        return newS;
+    }//end wordToPigLatin
+
+    public static int findStartOfEndingPuncSeq(String s) {
+        s = s.toLowerCase();
+        int index = 0;
+        String[] letter = {".", "-", "!", "?", ",", ";"};
+        int length = s.length();
+        for(int i=0; i<length; i++) {
+            for(int j=0; j<6; j++) {
+                String sub = s.substring(i, i+1);
+
+                if(s.contains(letter[j]) && sub.contains(letter[j])) {
+                    index = i;
+                    break;
+                }
+                else {
+                    index = -1;
+                }
+                if(index != -1) {
+                    break;
+                }
+            }
+            if(index != -1) {
+                break;
+            }
         }
 
+        return index;
+    }//end findStartOfEndingPuncSeq
 
-
-
-        return pigLatin;
-    }
+    public static void fileToPigLatin(String s) throws FileNotFoundException {
+        String x, write;
+        Scanner reader = new Scanner (new File (s));
+        PrintWriter writer = new PrintWriter(new File("file_PL.txt"));
+        while(reader.hasNext()) {
+            x = reader.next();
+            //if doesn't end in punctuation
+            if(findStartOfEndingPuncSeq(x)==-1) {
+                write = wordToPigLatin(x);
+                write += " ";
+                writer.write(write);
+            }
+            else {
+                String sub = x.substring(findStartOfEndingPuncSeq(x));
+                if(sub.equals(x)) {
+                    write = x + " ";
+                    writer.write(write);
+                }
+                else {
+                    x = x.replaceAll(sub, "");
+                    write = wordToPigLatin(x);
+                    write = write + sub + " ";
+                    writer.write(write);
+                }
+            }
+        }
+        writer.close();
+    }//end fileToPigLatin
 }
