021c582d6c4febbf4b73faeff3cd4848a622e7ee
Sujay Swain <sujaymswain@gmail.com>
2018-05-31 23:40:09 -0400

    more

---
+++ ADS Problem Set 09/src/net/ads/mrpaul/xc240/ps09/ChangeCalculator.java
@@ -0,0 +1,23 @@
+package net.ads.mrpaul.xc240.ps09;
+
+public class ChangeCalculator {
+
+    public static void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+    }
+
+    public static int recursiveMinChange(int n, int[] coins) {
+        int valueCheck = coins[coins.length-1];
+        if coins.
+    }
+}
