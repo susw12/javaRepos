0e987aefedc7ddfc4be7ac2a3047b7eabfe6f917
Sujay Swain <sujaymswain@gmail.com>
2018-06-03 10:04:39 -0400

    More.
    Need to get some stuff from Daniel

--- ADS Problem Set 09/src/net/ads/mrpaul/xc240/ps09/ChangeCalculator.java
+++ ADS Problem Set 09/src/net/ads/mrpaul/xc240/ps09/ChangeCalculator.java
@@ -7,7 +7,7 @@ public class ChangeCalculator {
         int n = arr.length;
         for (int i = 0; i < n-1; i++)
             for (int j = 0; j < n-i-1; j++)
-                if (arr[j] > arr[j+1])
+                if (arr[j] <= arr[j+1])
                 {
                     // swap temp and arr[i]
                     int temp = arr[j];
@@ -17,7 +17,9 @@ public class ChangeCalculator {
     }
 
     public static int recursiveMinChange(int n, int[] coins) {
-        int valueCheck = coins[coins.length-1];
-        if coins.
+        int valueCheck = coins[0];
+        if (n - valueCheck < 0) {
+
+        }
     }
 }
