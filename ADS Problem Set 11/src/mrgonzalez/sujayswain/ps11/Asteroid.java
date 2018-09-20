d8df83f9d32ea4a3511be4f7752587d0fa2af923
Sujay Swain <sujaymswain@gmail.com>
2018-09-19 22:54:14 -0400

    Ya
    Now time to get on to making asteroids and  bullets

--- ADS Problem Set 11/src/mrgonzalez/sujayswain/ps11/Asteroid.java
+++ ADS Problem Set 11/src/mrgonzalez/sujayswain/ps11/Asteroid.java
@@ -1,8 +1,37 @@
-package mrgonzalez.sujayswain.ps11;
 
-public class Asteroid extends Polygon{
+package mrgonzalez.sujayswain.ps11;
+import java.io.*;
+import java.util.*;
+
+import mrgonzalez.sujayswain.ps11.Point;
+
+import java.awt.*;
+
+public class Asteroid extends Polygon {
     public Asteroid(Point[] inShape, Point inPosition, double inRotation) {
         super(inShape, inPosition, inRotation);
     }
 
+    public void paint(Graphics brush) {
+        Point[] p = this.getPoints();
+        int[] px = new int[p.length];
+        int[] py = new int[p.length];
+
+        for(int i=0; i<p.length; i++) {
+            px[i] = (int) p[i].getX();
+            py[i] = (int) p[i].getY();
+        }
+        brush.fillPolygon(px, py, p.length);
+    }
+
+    public void move() {
+        int stepSize = 1;
+        this.position.setX(this.position.getX() + stepSize * Math.cos(Math.toRadians(rotation)));
+        this.position.setY(this.position.getY() + stepSize * Math.sin(Math.toRadians(rotation)));
+
+//wrapping around edges
+        if(this.position.x >= 800) this.position.x = 0;
+        if(this.position.y >= 600) this.position.y = 0;
+
+    }
 }
--- ADS Problem Set 11/src/mrgonzalez/sujayswain/ps11/Ship.java
+++ ADS Problem Set 11/src/mrgonzalez/sujayswain/ps11/Ship.java
@@ -1,99 +1,87 @@
-
 package mrgonzalez.sujayswain.ps11;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+/**
+ * Programs ship functions
+ *
+ * This program contains code for ship movement and other functions.
+ * <p>
+ * ADSB: Ship.java
+ * 9/19/2018
+ *
+ * @author Sujay Swain
+ */
+public class Ship extends Polygon implements KeyListener{
+    static boolean forward = false, left = false, right = false;
+    private Object width, height;
+    public Ship(Point[] inShape, Point inPosition, double inRotation) {
+        super(inShape, inPosition, inRotation);
+    }
 
-public class Ship extends GameObject {
-    private double turnSpeed = 180.0;
-    private double accelerateSpeed = 250.0;
-    private double slowingSpeed = .95;
-    private boolean leftTurning = false, rightTurning = false, acceling = false;
+    public void paint(Graphics brush) {
+        Point[] p = this.getPoints();
+        int[] px = new int[p.length];
+        int[] py = new int[p.length];
 
-    /**
-     * Creates Ship
-     *
-     * Creates ship within mesh and hitbox range. <p>
-     *
-     * @param x start x
-     * @param y start y
-     * @return Ship s
-     */
-    public Ship(double x, double y) {
-        this.x = x; this.y = y;
-        this.direction = 0;
-
-
-        int numPoints;
-        Point[] points;
-        double r, dir;
-
-        numPoints = 50;
-        points = new Point[numPoints];
-        for (int i = 0; i < numPoints; i++) {
-            dir = 2 * Math.PI * i / numPoints;
-            r = -30 *( Math.pow( Math.cos(5 * dir), 2 ) * Math.sin(dir) + 0.3);
-            double pointX = r * Math.cos(dir - Math.PI / 2);
-            double pointY = r * Math.sin(dir  - Math.PI / 2);
-            points[i] = new Point(pointX, pointY);
+        for(int i=0; i<p.length; i++) {
+            px[i] = (int) p[i].getX();
+            py[i] = (int) p[i].getY();
         }
-        this.mesh = new Polygon(points, new Point(x,y), 0);
+        move();
+        brush.drawPolygon(px, py, p.length);
+    }
 
-        numPoints = 50;
-        points = new Point[numPoints];
-        for (int i = 0; i < numPoints; i++) {
-            dir = 2 * Math.PI * i / numPoints;
-            r = -30 *( Math.pow( Math.cos(5 * dir), 2 ) * Math.sin(dir) + 0.3);
-            double pointX = r * Math.cos(dir - Math.PI / 2);
-            double pointY = r * Math.sin(dir  - Math.PI / 2);
-            points[i] = new Point(pointX, pointY);
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        switch(key) {
+            case KeyEvent.VK_UP:
+                forward = true;
+                break;
+            case KeyEvent.VK_RIGHT:
+                right = true;
+                break;
+            case KeyEvent.VK_LEFT:
+                left = true;
+                break;
         }
-        this.hitBox = new Polygon(points, new Point(x,y), 0);
-
-        setMaxR();
     }
 
-    public void leftTurn() {
-        leftTurning = true;
-        rightTurning = false;
-    }
-
-    public void stopleftTurn() {
-        leftTurning = false;
-    }
-
-    public void rightTurn() {
-        rightTurning = true;
-        leftTurning = false;
-    }
-
-    public void stoprightTurn() {
-        rightTurning = false;
-    }
-
-    public void accelerate() {
-        acceling = true;
-    }
-
-    public void stopAccelerate() {
-        acceling = false;
-    }
-
-    /**
-     * Ticks
-     *
-     * Accelerates, applies friction, and turns.
-     *
-     * <p>
-     *
-     * @param screenWidth width of screen
-     * @param screenHeight height of screen
-     *
-     */
-    public void tick(int screenWidth, int screenHeight) {
-        super.tick();
-        if (leftTurning) direction -= turnSpeed / 60;
-        if (rightTurning) direction += turnSpeed / 60;
-        if (acceling) {
-            this.xv += accelerateSpeed * Math.cos(Math.toRadians(direction)) / 60;
-            this.yv += accelerateSpeed * Math.sin(Math.toRadians(direction)) / 60;
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        switch(key) {
+            case KeyEvent.VK_UP:
+                forward = false;
+                break;
+            case KeyEvent.VK_RIGHT:
+                right = false;
+                break;
+            case KeyEvent.VK_LEFT:
+                left = false;
+                break;
         }
+    }
+
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public void move() {
+//moving forward
+        int stepSize = 3;
+        if(forward) {
+            this.position.x = this.position.getX() + stepSize * Math.cos(Math.toRadians(rotation));
+            this.position.y = this.position.getY() + stepSize * Math.sin(Math.toRadians(rotation));
+        }
+
+//wrapping around edges
+        if(this.position.x >= 800) this.position.x = 10;
+        if(this.position.y >= 600) this.position.y = 0;
+        if(this.position.x <= 0) this.position.x = 800;
+        if(this.position.y <= 0) this.position.y = 600;
+
+//rotate
+        if(right) this.rotation += 2;
+        if(left) this.rotation -= 2;
     }
 }
