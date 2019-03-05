ea56108d046768f027cd1d058f61b6aabfa9040b
Sujay Swain <sujaymswain@gmail.com>
2019-03-05 10:09:04 -0500

    Revert "WTF gonzo"
    This reverts commit bfa899ad9cd1ec3044734c2d2285c1e678faff5d.

--- AoAExercise2/src/net/mr/gonzalez/exercise2/MyHashSet.java
+++ AoAExercise2/src/net/mr/gonzalez/exercise2/MyHashSet.java
@@ -1,7 +1,5 @@
 package net.mr.gonzalez.exercise2;
 
-import javafx.scene.Node;
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -9,30 +7,24 @@ public class MyHashSet<E> extends java.lang.Object implements Iterable<E>{
     public static final int DEFAULT_INITIAL_CAPACITY = 10;
     public static final double LOAD_FACTOR = 10;
     private static final int PRIME = 223063091;
-    private Node[] hashTable;
+    private E[] hashTable;
     private int size;
 
     @SuppressWarnings("unchecked")
     public MyHashSet() {
         size = 0;
-        hashTable = new Node[DEFAULT_INITIAL_CAPACITY];
+        hashTable = (E[]) new Object[DEFAULT_INITIAL_CAPACITY];
     }
 
     @SuppressWarnings("unchecked")
     public MyHashSet(int initialCapacity){
         size = 0;
-        hashTable = new Node[initialCapacity];
+        hashTable = (E[]) new Object[initialCapacity];
     }
 
     @SuppressWarnings("unchecked")
     public void add(E element) {
         if (!this.contains(element)) {
-            int position = (PRIME * element.hashCode()) % hashTable.length;
-            while (hashTable[position] != null) {
-                position++;
-            }
-            hashTable[position] = element;
-            size++;
             if (this.getCapacity()*LOAD_FACTOR <= this.getSize()+1) {
                 E[] replacementArray = (E[]) new Object[this.getCapacity()*2];
                 for (int x = 0; x < hashTable.length; x++) {
